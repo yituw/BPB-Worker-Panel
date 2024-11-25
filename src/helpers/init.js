57b9b73b52b9bbcc29d06160d7070a9cc5a66f17
yituw <51936905+yituw@users.noreply.github.com>
2024-11-25 14:53:38 +0800

    Update init.js

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -4,7 +4,7 @@ let userID, dohURL, proxyIP, trojanPassword, defaultHttpPorts, defaultHttpsPorts
 
 function initParams(request, env) {
     const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
-    userID = env.UUID || '89b3cbba-e6ac-485a-9481-976a0415eab9';
+    userID = env.UUID || '4aaa183c-3c5f-4b2f-a7f1-182a53e20e51';
     if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
     dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
     proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : defaultProxyIP;
